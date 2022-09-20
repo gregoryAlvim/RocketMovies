7b3943434092ac9fe57305ce2cd90defdae3ce25
Grégory Alvim <gregori.jagua@gmail.com>
2022-09-20 08:18:14 -0300

    Chore: created global app colors themes

---
+++ src/styles/theme/index.js
@@ -0,0 +1,15 @@
+export const theme = {
+  COLORS: {
+    BG_PRIMARY: '#1C1B1E',
+    BG_SECONDARY: '#262529',
+    BG_ROSE: '#FF859B',
+
+    COLOR_LINE: '#3E3B47',
+
+    TEXT_ROSE: '#FF859B',
+    TEXT_WHITE: '#F4EDE8',
+    TEXT_GRAY_100: '#CAC4CF',
+    TEXT_GRAY_200: '#948F99',
+    TEXT_GRAY_300: '#312E38',
+  },
+ }

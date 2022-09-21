17bbefdcf3da50f05ec9b252f6f0f7ac1abcfeb3
Grégory Alvim <gregori.jagua@gmail.com>
2022-09-21 13:41:07 -0300

    Refactor: added icon property on component Button

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 
 import { Container } from './styles';
 
-export function Button({title, loading = false, ...rest}) {
+export function Button({title, loading = false, icon: Icon, ...rest}) {
   return (
    <Container
       type='button'
       disabled={loading}
       {...rest}
    >
+      {Icon && <Icon size={20} />}
       { loading ? 'Carregando...' : title } 
    </Container>
   );

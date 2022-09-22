1476348fd0b3356b3fb4a257ae05fdb7354c51ab
Grégory Alvim <gregori.jagua@gmail.com>
2022-09-22 17:37:39 -0300

    Chore: created new component Header

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,25 @@
+import { Input } from '../Input';
+
+import { Container, Profile } from './styles';
+
+export function Header() {
+   return (
+      <Container>
+         <h1> RocketMovies </h1>
+
+         <Input 
+            type='text'
+            placeholder='Pesquisar pelo título'
+         />
+
+         <Profile>
+            <div>
+               <strong> Grégory Alvim </strong>
+               <span>sair</span>
+            </div>
+
+            <img src="https://github.com/gregoryAlvim.png" alt="Foto de perfil do usuário logado no RocketMovies" />
+         </Profile>
+      </Container>
+   );
+}

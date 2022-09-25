77bded62ca708a24d2dd6e4cfa61709e42f20d8a
Grégory Alvim <gregori.jagua@gmail.com>
2022-09-25 16:36:36 -0300

    Feat: created new app routes to new pages

--- src/routes/app.routes.jsx
+++ src/routes/app.routes.jsx
@@ -1,10 +1,14 @@
 import { Route, Routes } from 'react-router-dom';
 import { Home } from '../pages/Home';
+import { Profile } from '../pages/Profile';
+import { Details } from '../pages/Details';
 
 export function AppRoutes () {
    return (
       <Routes>
          <Route path="/" element={<Home />} />
+         <Route path="/profile" element={<Profile />} />
+         <Route path="/detailMovie/:id" element={<Details />} />
       </Routes>
    );
 }

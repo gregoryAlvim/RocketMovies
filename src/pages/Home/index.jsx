9fa454acae8f4d7615036a054f570b851c105676
Grégory Alvim <gregori.jagua@gmail.com>
2022-09-28 23:08:41 -0300

    Feat: created  app home page

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,69 @@
+import { Link } from 'react-router-dom';
+import { Header } from '../../components/Header';
+import { Button } from '../../components/Button';
+import { MovieCard } from '../../components/MovieCard';
+
+import { FiPlus } from 'react-icons/fi';
+import { Container, MyMovies, AddMovies, Movies } from './styles';
+
+export function Home() {
+   return (
+      <Container>
+         <Header />
+
+         <MyMovies>
+            <h1> Meus Filmes </h1>
+
+            <Link to="create-new-movie">
+               <AddMovies>
+                  <Button
+                     type="button"
+                     title="Adicionar filme"
+                     icon={FiPlus}
+                  />
+               </AddMovies>
+            </Link>
+         </MyMovies>
+
+         <Movies>
+            <Link to="detailMovie/1">
+               <MovieCard key={1} id="FirstMovieCard" data={{
+                  title: 'The spider man',
+                  
+                  ratingStars: 4,
+                  description: 'Pragas nas colheitas fizeram a civilização humana regredir para uma sociedade agrária em futuro de data desconhecida. Cooper, ex-piloto da NASA, tem uma fazenda com sua família. Murphy, a filha de dez anos de Cooper, acredita que seu quarto está assombrado por um fantasma que tenta se Pragas nas colheitas fizeram a civilização humana regredir para uma sociedade agrária em futuro de data desconhecida. Cooper, ex-piloto da NASA, tem uma fazenda com sua família. Murphy, a filha de dez anos de Cooper, acredita que seu quarto está assombrado por um fantasma que tenta se ',
+                  tags: [
+                     { id: '1', name: 'Drama' },
+                     { id: '2', name: 'Ação' },
+                     { id: '3', name: 'Super-Herói' },
+                  ]
+               }} />
+            </Link>
+
+            <Link to="detailMovie/1">
+               <MovieCard key={2} data={{
+                  title: 'A cabana',
+                  ratingStars: 3,
+                  description: 'Pragas nas colheitas fizeram a civilização humana regredir para uma sociedade agrária em futuro de data desconhecida. Cooper, ex-piloto da NASA, tem uma fazenda com sua família. Murphy, a filha de dez anos de Cooper, acredita que seu quarto está assombrado por um fantasma que tenta se Pragas nas colheitas fizeram a civilização humana regredir para uma sociedade agrária em futuro de data desconhecida. Cooper, ex-piloto da NASA, tem uma fazenda com sua família. Murphy, a filha de dez anos de Cooper, acredita que seu quarto está assombrado por um fantasma que tenta se ',
+                  tags: [
+                     { id: '1', name: 'Suspense' },
+                     { id: '2', name: 'Terror' },
+                  ]
+               }} />
+            </Link>
+
+            <Link to="detailMovie/1">
+               <MovieCard key={3} data={{
+                  title: 'A cabana',
+                  ratingStars: 3,
+                  description: 'Pragas nas colheitas fizeram a civilização humana regredir para uma sociedade agrária em futuro de data desconhecida. Cooper, ex-piloto da NASA, tem uma fazenda com sua família. Murphy, a filha de dez anos de Cooper, acredita que seu quarto está assombrado por um fantasma que tenta se Pragas nas colheitas fizeram a civilização humana regredir para uma sociedade agrária em futuro de data desconhecida. Cooper, ex-piloto da NASA, tem uma fazenda com sua família. Murphy, a filha de dez anos de Cooper, acredita que seu quarto está assombrado por um fantasma que tenta se ',
+                  tags: [
+                     { id: '1', name: 'Suspense' },
+                     { id: '2', name: 'Terror' },
+                  ]
+               }} />
+            </Link>
+         </Movies>
+      </Container>
+   );
+}

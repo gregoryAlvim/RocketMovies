3b2a03c35902e3bf5c2f436d639387fc04129322
Grégory Alvim <gregori.jagua@gmail.com>
2022-10-01 11:30:17 -0300

    Feat: little changes

--- src/components/BackButton/styles.js
+++ src/components/BackButton/styles.js
@@ -2,6 +2,7 @@ import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 export const Container = styled.div`
+   width: 9rem;
 `;
 
 export const Back = styled(Link)`
@@ -9,7 +10,8 @@ export const Back = styled(Link)`
    align-items: center;
    gap: 0.8rem;
 
-   height: 11.6rem;
+   height: 11.6rem;  
+
    color: ${ ({ theme }) => theme.COLORS.BG_ROSE };
    text-decoration: none;
 `;

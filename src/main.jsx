0968d37e3579ab41ab64a1f92b54eefd8f303078
Grégory Alvim <gregori.jagua@gmail.com>
2022-10-20 23:08:51 -0300

    Refactor: added authprovider

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-import { Routes}  from './routes';
-import GlobalStyles from './styles/global';
 
-import { ThemeProvider } from 'styled-components';
+import { Routes}  from './routes';
 import { theme } from './styles/theme';
+import { AuthProvider } from './hooks/auth';
+import { ThemeProvider } from 'styled-components';
+
+import GlobalStyles from './styles/global';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Routes />
+      <AuthProvider>
+        <Routes />
+      </AuthProvider>
     </ThemeProvider>
   </React.StrictMode>
 )

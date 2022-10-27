0081bf960f804c16dd781700fe6935f9cf0fb66d
Grégory Alvim <gregori.jagua@gmail.com>
2022-10-26 22:59:48 -0300

    Feat: initialized work to render movie cards from api

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,4 +1,8 @@
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+
+import { api } from "../../services/api";
+
 import { Header } from '../../components/Header';
 import { Button } from '../../components/Button';
 import { MovieCard } from '../../components/MovieCard';
@@ -7,9 +11,26 @@ import { FiPlus } from 'react-icons/fi';
 import { Container, MyMovies, AddMovies, Movies } from './styles';
 
 export function Home() {
+
+   const [search, setSearch] = useState("");
+   const [movies, setMovies] = useState("");
+
+   useEffect(() => {
+      async function fetchMovies() {
+         const response = await api.get("movie-notes");
+         console.log(response.data);
+         setMovies(response.data);
+      }
+
+      fetchMovies();
+   }, []);
+
    return (
       <Container>
-         <Header />
+
+         <Header
+            onChange={event => setSearch(event.target.value)}
+         />
 
          <MyMovies>
             <h1> Meus Filmes </h1>
@@ -26,43 +47,21 @@ export function Home() {
          </MyMovies>
 
          <Movies>
-            <Link to="detailMovie/1">
-               <MovieCard key={1} id="FirstMovieCard" data={{
-                  title: 'The spider man',
-                  
-                  ratingStars: 4,
-                  description: 'Pragas nas colheitas fizeram a civilização humana regredir para uma sociedade agrária em futuro de data desconhecida. Cooper, ex-piloto da NASA, tem uma fazenda com sua família. Murphy, a filha de dez anos de Cooper, acredita que seu quarto está assombrado por um fantasma que tenta se Pragas nas colheitas fizeram a civilização humana regredir para uma sociedade agrária em futuro de data desconhecida. Cooper, ex-piloto da NASA, tem uma fazenda com sua família. Murphy, a filha de dez anos de Cooper, acredita que seu quarto está assombrado por um fantasma que tenta se ',
-                  tags: [
-                     { id: '1', name: 'Drama' },
-                     { id: '2', name: 'Ação' },
-                     { id: '3', name: 'Super-Herói' },
-                  ]
-               }} />
-            </Link>
+            {
+               <Link to="detailMovie/1">
+                  <MovieCard key={1} id="FirstMovieCard" data={{
+                     title: 'The spider man',
 
-            <Link to="detailMovie/1">
-               <MovieCard key={2} data={{
-                  title: 'A cabana',
-                  ratingStars: 3,
-                  description: 'Pragas nas colheitas fizeram a civilização humana regredir para uma sociedade agrária em futuro de data desconhecida. Cooper, ex-piloto da NASA, tem uma fazenda com sua família. Murphy, a filha de dez anos de Cooper, acredita que seu quarto está assombrado por um fantasma que tenta se Pragas nas colheitas fizeram a civilização humana regredir para uma sociedade agrária em futuro de data desconhecida. Cooper, ex-piloto da NASA, tem uma fazenda com sua família. Murphy, a filha de dez anos de Cooper, acredita que seu quarto está assombrado por um fantasma que tenta se ',
-                  tags: [
-                     { id: '1', name: 'Suspense' },
-                     { id: '2', name: 'Terror' },
-                  ]
-               }} />
-            </Link>
-
-            <Link to="detailMovie/1">
-               <MovieCard key={3} data={{
-                  title: 'Lucy',
-                  ratingStars: 1,
-                  description: 'Pragas nas colheitas fizeram a civilização humana regredir para uma sociedade agrária em futuro de data desconhecida. Cooper, ex-piloto da NASA, tem uma fazenda com sua família. Murphy, a filha de dez anos de Cooper, acredita que seu quarto está assombrado por um fantasma que tenta se Pragas nas colheitas fizeram a civilização humana regredir para uma sociedade agrária em futuro de data desconhecida. Cooper, ex-piloto da NASA, tem uma fazenda com sua família. Murphy, a filha de dez anos de Cooper, acredita que seu quarto está assombrado por um fantasma que tenta se ',
-                  tags: [
-                     { id: '1', name: 'Ação' },
-                     { id: '2', name: 'Futurista' },
-                  ]
-               }} />
-            </Link>
+                     ratingStars: 4,
+                     description: 'Pragas nas colheitas fizeram a civilização humana regredir para uma sociedade agrária em futuro de data desconhecida. Cooper, ex-piloto da NASA, tem uma fazenda com sua família. Murphy, a filha de dez anos de Cooper, acredita que seu quarto está assombrado por um fantasma que tenta se Pragas nas colheitas fizeram a civilização humana regredir para uma sociedade agrária em futuro de data desconhecida. Cooper, ex-piloto da NASA, tem uma fazenda com sua família. Murphy, a filha de dez anos de Cooper, acredita que seu quarto está assombrado por um fantasma que tenta se ',
+                     tags: [
+                        { id: '1', name: 'Drama' },
+                        { id: '2', name: 'Ação' },
+                        { id: '3', name: 'Super-Herói' },
+                     ]
+                  }} />
+               </Link>
+            }
          </Movies>
       </Container>
    );

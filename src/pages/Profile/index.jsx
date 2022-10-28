2051704b29a71b57c8d9dab58f6366592b47a84c
Grégory Alvim <gregori.jagua@gmail.com>
2022-10-27 23:13:47 -0300

    Feat: added avatar on update user profile

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -25,14 +25,16 @@ export function Profile() {
 
    async function handleUpdate() {
       
-      const user = {
+      const updated = {
          name,
          email,
          password: newPassword,
          old_password: oldPassword,
       };
 
-      await updateProfile({ user, avatarFile });
+      const userUpdated = Object.assign(user, updated)
+
+      await updateProfile({ user: userUpdated, avatarFile });
    }
 
    function handleChangeAvatar(event) {

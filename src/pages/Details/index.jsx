32bd0a2258559d07723452524a828ed93c8299f7
Grégory Alvim <gregori.jagua@gmail.com>
2022-10-27 23:12:24 -0300

    Feat: added data details movies from api

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,60 +1,82 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+
+import { api } from "../../services/api";
+import { useAuth } from "../../hooks/auth";
+
 import { Header } from '../../components/Header';
 import { BackButton } from '../../components/BackButton';
 import { Rating } from '../../components/Rating';
 import { Tag } from '../../components/Tag';
 
-import { usersData } from '../../utils/users';
-import { data } from '../../utils/datas';
-
 import { MdOutlineWatchLater } from 'react-icons/md';
 import { Container, Content, BoxTitleAndRating, Title, BoxCreatedAt, Registered, Tags, Description } from './styles';
 
 export function Details() {
+
+   const { user } = useAuth();
+
+   const params = useParams();
+   const avatarURL = user.avatar ? `${api.defaults.baseURL}/files/${user.avatar}` : avataPlaceholder;
+
+
+   const [movie, setMovie] = useState(null);
+
+   useEffect(() => {
+      async function fetchMovie() {
+         const response = await api.get(`movie-notes/${params.id}`);
+         setMovie(response.data);
+      }
+
+      fetchMovie();
+
+   }, []);
+
    return (
       <Container>
          <Header />
+
          <Content>
-               <BackButton className='BackButtonDetails' />
-            <main>
+            <BackButton className='BackButtonDetails' />
 
+            {
+               movie && 
+               <main>
+                  <BoxTitleAndRating>
+                     <Title> {movie.title} </Title>
 
+                     <Rating className="detailsRating" rating={4} />
+                  </BoxTitleAndRating>
 
-               <BoxTitleAndRating>
-                  <Title> {data.movie.title} </Title>
+                  <BoxCreatedAt>
+                     <Registered>
+                        <img src={avatarURL} alt="Foto de perfil do usuário logado no RocketMovies" />
 
-                  <Rating className="detailsRating" rating={4} />
-               </BoxTitleAndRating>
+                        Por {user.name}
+                     </Registered>
 
-               <BoxCreatedAt>
-                  <Registered>
-                     <img src="https://github.com/gregoryAlvim.png" alt="Foto de perfil do usuário logado no RocketMovies" />
+                     <Registered>
+                        <MdOutlineWatchLater size={25} />
 
-                     Por {usersData.user1.name}
-                  </Registered>
+                        {movie.created_at}
+                     </Registered>
+                  </BoxCreatedAt>
+                  <Tags>
+                     {
+                        movie.tags &&
+                        <footer>
+                           {
+                              movie.tags.map(tag => <Tag key={tag.id} title={tag.name} />)
+                           }
+                        </footer>
+                     }
+                  </Tags>
 
-                  <Registered>
-                     <MdOutlineWatchLater size={25} />
-
-                     23/05/22 às 08:00
-                  </Registered>
-               </BoxCreatedAt>
-               <Tags>
-                  {
-                     data.movie.tags &&
-                     <footer>
-                        {
-                           data.movie.tags.map(tag => <Tag key={tag.id} title={tag.name} />)
-                        }
-                     </footer>
-                  }
-               </Tags>
-
-               <Description>
-                  {data.movie.description}
-                  <br /><br />
-                  {data.movie.description}
-               </Description>
-            </main>
+                  <Description>
+                     {movie.description}
+                  </Description>
+               </main>
+            }
          </Content>
       </Container>
    );
